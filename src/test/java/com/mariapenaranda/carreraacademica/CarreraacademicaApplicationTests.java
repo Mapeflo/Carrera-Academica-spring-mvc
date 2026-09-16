2ff3e9644829bdce9e79783131ee35be3cfb15a8
mapeflo <mpenarandaf@unicartagena.edu.co>
2026-09-16 10:58:28 -0500

    1. Proyecto Spring Boot inicial creado desde start.spring.io

---
+++ src/test/java/com/mariapenaranda/carreraacademica/CarreraacademicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariapenaranda.carreraacademica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarreraacademicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
