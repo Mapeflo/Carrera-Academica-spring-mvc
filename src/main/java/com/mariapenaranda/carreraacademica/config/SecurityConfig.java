5a4c432a2e3d923e5f30f45570c98ef2a5d8aadc
mapeflo <mpenarandaf@unicartagena.edu.co>
2026-09-18 16:54:05 -0500

    Actualizar SecurityConfig

--- src/main/java/com/mariapenaranda/carreraacademica/config/SecurityConfig.java
+++ src/main/java/com/mariapenaranda/carreraacademica/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/carreras", true)
+                        .defaultSuccessUrl("/", true)
                         .permitAll()
                 )
                 .logout(logout -> logout
